1a97ca9503fac9bb652d72b22ad1aec2c21098d8
Bond <shrekuu@gmail.com>
2024-12-18 11:38:13 +0800

    feat: add github actions; config next.js to use github pages

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,23 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  output: 'standalone',
-  eslint: {
-    // disable linting during build phase
-    ignoreDuringBuilds: true,
+  output: "export",
+  images: {
+    unoptimized: true, // Disable default image optimization
   },
-  typescript: {
-    // disable type-checking during build phase
-    ignoreBuildErrors: true,
-  },
+  assetPrefix: isProd ? "/aspecta-realtime/" : "",
+  basePath: isProd ? "/aspecta-realtime" : "",
+  // eslint: {
+  //   // disable linting during build phase
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   // disable type-checking during build phase
+  //   ignoreBuildErrors: true,
+  // },
 };
 
 export default nextConfig;
