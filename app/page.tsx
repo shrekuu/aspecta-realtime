9155c0e1944540c10a514810b72287c9fae46c27
Bond <shrekuu@gmail.com>
2024-12-30 18:56:01 +0800

    feat: add Sphere X BuildKey Season 2

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import { useBalance } from 'wagmi';
 
 type TProject = {
   name: string;
+  client_name: string;
   display_name: string;
   address: Address;
   pool_address: Address;
@@ -24,6 +25,7 @@ type TProject = {
 const projects: TProject[] = [
   {
     name: 'memedisco',
+    client_name: 'memedisco',
     display_name: 'MEME DISCO',
     address: '0x5fCD873764c634e86D964D6004c3Ee3Ad93ce13a',
     pool_address: '0x446942015bE4983c7414256603E23F7E5Ea8D04e',
@@ -35,7 +37,21 @@ const projects: TProject[] = [
     },
   },
   {
+    name: 'sphere_x_build-key_season_2',
+    client_name: 'sphere-x',
+    display_name: 'Sphere X BuildKey Season 2',
+    address: '0x31BD42400597E2770A18bA112DFbB7894e0E4814',
+    pool_address: '0x198E5632014B34677DCaa73a86814393868378D0',
+    links: {
+      website: 'https://www.sx.xyz/',
+      x: 'https://x.com/SphereX_XYZ',
+      discord: 'https://discord.com/invite/spherex',
+      tg: 'https://t.me/SphereXExchange',
+    },
+  },
+  {
     name: 'yuliverse',
+    client_name: 'yuliverse',
     display_name: 'Yuliverse',
     address: '0x880b5AcDc2fACfaeF9770986E314DBd32999b328',
     pool_address: '0x42090e082782e1f3Aa438b05AB0742EF8054F667',
@@ -47,6 +63,7 @@ const projects: TProject[] = [
   },
   {
     name: 'junlala',
+    client_name: 'junlala',
     display_name: 'JunLaLa',
     address: '0x5a3D65C67e6bF2936590eF56736a567C8Ef83C1C',
     pool_address: '0x02e72278c77874A2aBAe33d57C135956cF32dF59',
@@ -58,6 +75,7 @@ const projects: TProject[] = [
   },
   {
     name: 'codatta',
+    client_name: 'codatta',
     display_name: 'Codatta',
     address: '0x24c7ed5F8D5896B9B3Ea294edD2e41dA622eEF93',
     pool_address: '0x82012CE154108959729BA98F2d65DF809b72eE21',
@@ -70,6 +88,7 @@ const projects: TProject[] = [
   },
   {
     name: 'liveart',
+    client_name: 'liveart',
     display_name: 'LiveArt',
     address: '0xf9521AE0482Fdb271816739a6A6Cb6e3C8699deA',
     pool_address: '0x437BeeF85ED71587d634d8Fdb93db3103691CB4D',
@@ -82,6 +101,7 @@ const projects: TProject[] = [
   },
   {
     name: 'moongate',
+    client_name: 'moongate',
     display_name: 'Moongate',
     address: '0x461Cb9448ea9D720192AFeD38244738E5Da7985A',
     pool_address: '0xb13Efac99553301a234Aab3Ee001B69b116d4DCa',
@@ -93,6 +113,7 @@ const projects: TProject[] = [
 
   {
     name: 'pandatitan',
+    client_name: 'pandatitan',
     display_name: 'Pandatitan',
     address: '0xb8A6B6670c989acdfCf27D55Cf2Eb7108Ff8F3d0',
     pool_address: '0xF852D23b4c0F4C95811BFC4F0962bE633bd9E698',
@@ -105,6 +126,7 @@ const projects: TProject[] = [
 
   {
     name: 'darenmarket',
+    client_name: 'darenmarket',
     display_name: 'DarenMarket',
     address: '0xCE8545F567A43d676F845286A0D9a38b204656f0',
     pool_address: '0x4a3286a7328DbF21075A604c4dcE443074CdCE81',
@@ -118,6 +140,7 @@ const projects: TProject[] = [
 
   {
     name: 'artela',
+    client_name: 'artela',
     display_name: 'Artela',
     address: '0xF250AF74d5D55F6251e743976596b8A923DD1d91',
     pool_address: '0x9721C8039FB930914D2f78DCdFBb9F83422655fb',
@@ -130,6 +153,7 @@ const projects: TProject[] = [
 
   {
     name: '4everland',
+    client_name: '4everland',
     display_name: '4EVERLAND',
     address: '0xD390262AF015b12Ff2Fca6A0Fe6E2D0D2893278d',
     pool_address: '0x3B3A5A3B312d3eBcc34e13aC6693B36E075DF2d9',
@@ -143,6 +167,7 @@ const projects: TProject[] = [
 
   {
     name: 'nubila-network',
+    client_name: 'nubila-network',
     display_name: 'Nubila Network',
     address: '0x59A3d8b77A3A2C626d4769D7FEf741fF5064878B',
     pool_address: '0x92593C21443102ebdd0b2F8523c082556dDE3C06',
@@ -155,6 +180,7 @@ const projects: TProject[] = [
 
   {
     name: 'codexfield',
+    client_name: 'codexfield',
     display_name: 'CodexField',
     address: '0x6238d3bf1D69FF15D0fa804181479c24b23Fe85D',
     pool_address: '0x6647AB15d5c843ba8A27952E3BD04c7221C9f77F',
@@ -167,6 +193,7 @@ const projects: TProject[] = [
 
   {
     name: 'gaib',
+    client_name: 'gaib',
     display_name: 'GAIB',
     address: '0x833681102391636fa640F040b53FFf948fbAc508',
     pool_address: '0x659dEbA66FC777aa86396636f7b611093B7a0Ab8',
@@ -180,6 +207,7 @@ const projects: TProject[] = [
 
   {
     name: 'sphere-x',
+    client_name: 'sphere-x',
     display_name: 'Sphere X',
     address: '0xD134A126251B0E16854B1419bb89B90A9514703B',
     pool_address: '0xde5AFaD8AD0dAEea283C9805A99bF7223E1E1397',
@@ -192,6 +220,7 @@ const projects: TProject[] = [
   },
   {
     name: 'breadnbutter',
+    client_name: 'breadnbutter',
     display_name: 'BreadnButter',
     address: '0x1B768D35DA7232661385Cbb7fB871238E31D6307',
     pool_address: '0xA95BF5d827a58cbB056cBB41174B6A2E0220D9Fc',
@@ -215,7 +244,7 @@ const ProjectRow = ({ project }: { project: TProject }) => {
       <td className="border-b border-slate-100 p-4 pl-8 text-slate-500 dark:border-slate-700 dark:text-slate-400">
         <a href="https://www.moongate.id" target="_blank" className="link-with-arrow inline-flex items-center gap-2 hover:text-cyan-500">
           <div className="flex-none rounded bg-slate-200">
-            <Image src={`/images/index/project-logos/${project.name}.png`} width={30} height={30} alt={project.display_name} className="rounded" />
+            <Image src={`/images/index/project-logos/${project.client_name}.png`} width={30} height={30} alt={project.display_name} className="rounded" />
           </div>
 
           <div>{project.display_name}</div>
